22f9c2a38d3b17bf1dfe3b23ccf4ae3f44faa504
Erick Pariona Yauricasa <erick.pariona@hotmail.com>
2024-02-01 01:55:45 -0500

    config

--- src/main/java/com/servicio/epy/productos/SbServicioProductosApplication.java
+++ src/main/java/com/servicio/epy/productos/SbServicioProductosApplication.java
@@ -2,7 +2,10 @@ package com.servicio.epy.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class SbServicioProductosApplication {
 
